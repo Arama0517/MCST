37560581afad994a90d3cac16d0102314acd9b87
Arama <3584075812@qq.com>
2024-05-17 13:18:41 +0800

    ✨ feat(pages): 开始编写页面

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/Arama-Vanarana/MCSCS-Go/lib"
+	"github.com/Arama-Vanarana/MCSCS-Go/pages"
 )
 
 var Logger = lib.Logger
@@ -16,5 +17,18 @@ func main() {
 		fmt.Println(lib.VERSION)
 		return
 	}
-	lib.Download("https://dl.google.com/go/go1.14.2.src.tar.gz", "go1.14.2.src.tar.gz")
+	options := []string{"创建服务器", "退出"}
+	for {
+		selected := lib.Select(options, "请选择一个选项 ")
+		var err error
+		switch selected {
+		case 0:
+			err = pages.CreatePage()
+		default:
+			return
+		}
+		if err != nil {
+			Logger.WithError(err).Error("运行失败")
+		}
+	}
 }
---
+++ pages/create.go
@@ -0,0 +1,10 @@
+package pages
+
+// func jvm_args_XMS() (int, error) {
+// 	// options := []string{""}
+// 	return 0, nil
+// }
+
+func CreatePage() error {
+	return nil
+}
