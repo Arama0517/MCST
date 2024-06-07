5902293060c300c4282d0040eae741b7d5da9b77
Arama-Vanarana <122252361+Arama-Vanarana@users.noreply.github.com>
2024-06-07 18:33:50 +0800

    fix(页面): 修复启动页面没有的问题

--- cmd/root.go
+++ cmd/root.go
@@ -51,6 +51,6 @@ func newRootCmd(version string) *cobra.Command {
 	}
 	cmd.SetVersionTemplate("{{.Version}}")
 	cmd.PersistentFlags().BoolVar(&verbose, "debug", false, "调试模式(更多的日志)")
-	cmd.AddCommand(newCreateCmd(), newDownloadCmd(), newConfigCmd(), newListCmd(), newManCmd())
+	cmd.AddCommand(newCreateCmd(), newDownloadCmd(), newConfigCmd(), newStartCmd(), newListCmd(), newManCmd())
 	return cmd
 }
